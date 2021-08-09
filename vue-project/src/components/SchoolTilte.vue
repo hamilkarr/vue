dd4369993a97a08c3fd07563885d4a761e81b2ea
hamilkarr <12uclock@gmail.com>
2021-08-09 18:22:58 +0900

    08-09

---
+++ vue-project/src/components/SchoolTilte.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tilte: "학교 홈페이지 제목..."
+    };
+  }
+}
+</script>
---
+++ vue-project/src/views/school.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>School.vue</h1>
+  <h2 @click="changeTitle">{{ title }}</h2>
+  <Scooltitle />
+</template>
+
+<script>
+import SchoolTitle from "../components/SchoolTilte.vue"
+export default {
+  compnents :{SchoolTitle},
+  data() {
+    return {
+      title : "학교 홈페이지"
+    }
+  },
+  created() {
+    console.log("Created");
+  },
+  mounted() {
+    console.log("Mounted");
+  },
+  unmounted() { 
+    console.log("Unmounted");
+  },
+  methods : {
+    changeTitle(){
+      this.changeTitle2();
+    },
+    changeTitle2() {
+      this.title = "(변경)학교 홈페이지";
+    }
+  }
+}
+</script>
